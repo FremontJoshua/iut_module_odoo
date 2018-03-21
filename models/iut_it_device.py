c7d248afbc8118bf4170a7a85bf5a94eae684223
FremontJoshua <fremont.joshua85@gmail.com>
2018-03-21 16:13:16 +0100

    form + date garantie end

--- models/iut_it_device.py
+++ models/iut_it_device.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import datetime
 
 class IutItDevice(models.Model):
     _name = 'iut.it.device'
@@ -8,15 +9,15 @@ class IutItDevice(models.Model):
     name = fields.Char(required=True)
     date_allocation = fields.Date()
     serial_number = fields.Char(required=True)
-    date_purchase = fields.Date()
+    date_purchase = fields.Date(default=datetime.datetime.today())
     date_warranty_end = fields.Date()
     model_id = fields.Many2one('iut.it.model', string='Model')
     partner_id = fields.Many2one('res.partner', string='Partner')
-    room_id = fields.Integer(related='partner_id.room_id.id', store=True)
-    office_id = fields.Integer(related='partner_id.room_id.office_id.id', store=True)
-    #date_warranty_end = fields.Date()
+    room_id = fields.Integer(related='partner_id.room_id.id', store=True, string='Room')
+    office_id = fields.Integer(related='partner_id.room_id.office_id.id', store=True, string='Office')
 
 
-    # @api.onchange('date_purchase', 'model_id.brand_id.warranty_delay_month')
-    # def _change_date_warranty(self):
-    #     self.date_warranty_end = self.date_purchase + self.model_id.brand_id.warranty_delay_month
+    @api.onchange('date_purchase', 'model_id', 'model_id.brand_id.warranty_delay_month')
+    def _change_date_warranty(self):
+        self.date_warranty_end = (datetime.datetime.strptime(self.date_purchase, '%Y-%m-%d') +
+                                  datetime.timedelta(self.model_id.brand_id.warranty_delay_month * 365/12)).strftime('%Y-%m-%d')
--- models/iut_office.py
+++ models/iut_office.py
@@ -6,9 +6,13 @@ class IutOffice(models.Model):
     _name = 'iut.office'
     _inherit = 'res.partner'
 
-    partner_ids = fields.One2many('iut.room', 'office_id', string='Rooms')
+    room_ids = fields.One2many('iut.room', 'office_id', string='Rooms')
     employee_nb = fields.Integer(compute='_compute_employee_nb')
 
     @api.depends()
     def _compute_employee_nb(self):
         return True
+
+    @api.depends('is_company','parent_id.commercial_partner_id')
+    def _compute_commercial_partner(self):
+        pass
